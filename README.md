8dc2614ec648aad9aedff1805d02433ef9565bef
Ojkee <17oju17@gmail.com>
2024-12-14 18:49:34 +0100

    7

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 - [x] Day 1
 - [x] Day 2
 - [x] Day 3
-- [ ] Day 4
+- [x] Day 4
 - [x] Day 5
-- [ ] Day 6
+- [ ] Day 6 part 2
+- [x] Day 7
+- [ ] Day 8
--- bin/main.ml
+++ bin/main.ml
@@ -6,10 +6,11 @@ let () =
   (* Day3.part1 (); *)
   (* Day3.part2 () *)
   (* Day4.part1 () *)
-  Day4.part2 ()
+  (* Day4.part2 () *)
+  (* Day5.part1 () *)
+  (* Day5.part2 () *)
+  (* Day6.part1 () *)
 
-(* Day5.part1 () *)
-(* Day5.part2 () *)
-(* Day6.part1 () *)
-
-(* Day6.part2 () *)
+  (* Day6.part2 () *)
+  (* Day7.part1 () *)
+  Day7.part2 ()
